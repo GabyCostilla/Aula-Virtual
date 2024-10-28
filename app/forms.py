0a057798d6a3db9cebc4e4c4161f413974f7f1dd
GabyCostilla <104600696+GabyCostilla@users.noreply.github.com>
2024-10-28 16:22:51 -0300

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -28,5 +28,5 @@ class TaskForm(FlaskForm):
     submit = SubmitField('Añadir Tarea')
 
 class ProfileUpdateForm(FlaskForm):
-    picture = FileField('Actualizar Foto de Perfil', validators=[DataRequired()])  # Campo para la carga de la foto
-    submit = SubmitField('Actualizar Foto')  # Botón de envío
+    picture = FileField('Actualizar Foto de Perfil', validators=[DataRequired()])
+    submit = SubmitField('Actualizar Foto')  
