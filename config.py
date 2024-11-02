741965522007cfe6ba45abae2aac522926fa4fc8
GabyCostilla <d47722877@alumnos.itr.edu.ar>
2024-11-02 19:42:57 -0300

    PUM BASEDATE EN CLEVER

--- config.py
+++ config.py
@@ -2,6 +2,14 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'mi_clave_secreta')
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://usuario_aula:password_seguro@localhost/aula_virtual_db'
+    
+    # Configura la URI de conexión utilizando las variables de entorno de Clever Cloud
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'MYSQL_ADDON_URI',
+        'mysql+pymysql://uwxekpvh8fps6jpj:6pXWq9c3e2ptjrVMfmhJ@bl1l7thyu07qhb4xsk7y-mysql.services.clever-cloud.com:3306/bl1l7thyu07qhb4xsk7y'
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
+
+# Imprimir la URI para verificar que se asigna correctamente (esto se debe hacer fuera de la clase)
+print("DATABASE URI:", Config.SQLALCHEMY_DATABASE_URI)
